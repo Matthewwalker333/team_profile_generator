f6320d6b0a427712c026b74a838f10878465c8b4
Matthew Walker <Matthewwalkermw64@gmail.com>
2021-11-20 18:43:19 -0800

    manager query and add team member function added

--- app.js
+++ app.js
@@ -17,3 +17,50 @@ const teamMembers = [];
 function start() {
     managerQuery();
 }
+
+function managerQuery() {
+    inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "What is the name of the team manager?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Team Manager's ID number:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Team Manager's email address:"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Team Manager's office number:"
+        }
+    ]).then(val => {
+        const manager = new Manager(val.name, val.id, val.email, val.officeNumber);
+        console.log(manager)
+        teamMembers.push(manager);
+        addTeamMember();
+    })
+};
+
+function addTeamMember() {
+    inquirer.prompt([{
+        type: "list",
+        name: "what_type",
+        message: "Add an engineer or intern to the team?",
+        choices: ["Engineer", "Intern", "Not at this time"]
+    }]).then(val => {
+
+        if (val.what_type === "Engineer") {
+            engineerQuery();
+        } else if (val.what_type === "Intern") {
+            internQuery();
+        } else {
+            createFile();
+        }
+    })
+}
